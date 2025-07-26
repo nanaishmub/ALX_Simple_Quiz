4f1f91c533968b09f250d50b53a96376abe3db5f
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-07-26 14:11:40 +0100

    Corrected comparison

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     checked = document.querySelector('quiz');
     userAnswer = checked.value;
 
-    if (userAnswer == correctAnswer){
+    if (userAnswer === correctAnswer){
         document.getElementById('feedback').textContent = "Correct! Well done.";
     } else {
         document.getElementById('feedback').textContent = "That's incorrect. Try again!";
